b36add4f4d688eb0f33aa5f05c74a40dda1af619
SurajSingh3650 <suraj27singh03@gmail.com>
2025-01-15 21:42:17 +0530

    average

---
+++ Average.java
@@ -0,0 +1,16 @@
+package Arrays;
+import java.util.*;
+public class Average {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+          
+        }
+        float avg=(float)sum/arr.length;
+        
+
+        System.out.println("Average of array is "+avg);
+    }
+}
