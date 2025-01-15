b0a86b1fec686aa4ace9abfae3c902de758701dc
SurajSingh3650 <suraj27singh03@gmail.com>
2025-01-15 20:49:34 +0530

    sum

---
+++ Sumelement.java
@@ -0,0 +1,12 @@
+package Arrays;
+import java.util.*;
+public class Sumelement {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.print(sum);
+    }
+}
