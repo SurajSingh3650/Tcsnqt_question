7868d1293005c6be90a1b775c8e64b1966d80e8b
SurajSingh3650 <suraj27singh03@gmail.com>
2025-01-15 19:44:50 +0530

    largestquestion

--- Largest.java
+++ Largest.java
@@ -1,7 +1,54 @@
+// Find the Largest element in an array
+
+// Problem Statement: Given an array, we have to find the largest element in the array.
+
+// Examples
+// Example 1:
+// Input:
+//  arr[] = {2,5,1,3,0};
+// Output:
+//  5
+// Explanation:
+//  5 is the largest element in the array. 
+
+// Example2:
+// Input:
+//  arr[] = {8,10,5,7,9};
+// Output:
+//  10
+// Explanation:
+//  10 is the largest element in the array. 
+
+
+
+
+
+
+
+
+
+
+
 package Arrays;
+
+import java.util.*;
 
 public class Largest {
     public static void main(String[] args) {
-        System.out.print("surajj singh");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+
     }
 }
