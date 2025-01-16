9c3cc9b96c0b5a7d12d341c7b2b7d770e162328b
SurajSingh3650 <suraj27singh03@gmail.com>
2025-01-16 17:33:41 +0530

    resolve some error

--- Reverse.java
+++ Reverse.java
@@ -1,8 +1,6 @@
 package Arrays;
 // Reverse a given Array
 
-
-
 // Problem Statement: You are given an array. The task is to reverse the array and print it. 
 
 // Examples:
@@ -18,24 +16,27 @@ package Arrays;
 // Explanation: Since the order of elements gets reversed the first element will occupy the fifth position, the second element occupies the fourth position and so on.
 public class Reverse {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
-        for(int i=arr.length-1;i>=0;i--){
-            System.out.print(arr[i]+" ");
-        }
+        int arr[] = { 1, 2, 3, 4, 5 };
+      int  n=arr.length;
+        //  for(in t  i=arr.len g th - 1; i> =0;i --){
+        //     System.out.print(arr [ i]+" ");
+        // }
+        reverseArray(arr, n);
+
 
     }
     static void reverseArray(int arr[], int n) {
         int[] ans = new int[n];
-        for (int i = n - 1; i >= 0; i--) {
+           for  (int i = n - 1; i >= 0; i--) {
            ans[n - i - 1] = arr[i];
         }
-        printArray(ans, n);}
+      }
 
-        
-        static void printArray(int ans[], int n) {
-            System.out.print("Reversed array is:- \n");
-            for (int i = 0; i < n; i++) {
-               System.out.print(ans[i] + " ");
-            }
+    
+        public void printArray(int ans[], int n) {
+        System.out.print("Reversed ar");
+             for(int i = 0; i < n; i++) {
+         System.out.print(ans[i]);
+       }
          }
 }
